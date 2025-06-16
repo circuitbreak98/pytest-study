9375e3f575978e71afdd94c2747aae4ecdbf8dea
SEONHO LEE <seonhol@google.com>
2025-06-16 15:48:50 +0900

    use fixture parametrize

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,19 @@
-from calculator import add
+import pytest
+from calculator import add, divide
 
-def test_add():
-    assert add(2,3)==5
+@pytest.fixture
+def sample_numbers():
+    return (10,2)
+
+def test_add_with_fixture(sample_numbers):
+    a, b = sample_numbers
+    assert add(a,b) == 12
+
+@pytest.mark.parametrize("a,b,expected", [
+    (6,3,2),
+    (10,2,5),
+    (9,3,3),
+    ])
+def test_divide_parametrize(a,b,expected):
+    assert divide(a,b) == expected
+
